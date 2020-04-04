52cdc33d3ff80fba13d864e894c344fa08a3c317
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-04 19:55:40 +0100

    finished up to stock count test

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -2,15 +2,23 @@ def pet_shop_name(name)
     name = @pet_shop[:name]
 end
 
-def total_cash(sum)
-    sum = @pet_shop[:admin][:total_cash]
+def total_cash(total)
+    total = @pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(total, value)
-    total[:admin][:total_cash] += value
+def add_or_remove_cash(total, amount)
+    total  [:admin][:total_cash] += amount
 end 
 
 
 def pets_sold(sold)
      sold = @pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pets, sold)
+    pets [:admin][:pets_sold] += sold
+end
+
+def stock_count(stock)
+    stock [:pets].length
 end
